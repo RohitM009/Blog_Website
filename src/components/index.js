a22ab3335065a9fad9b229526cbd7218fe998d4b
rohit <maske0901@gmail.com>
2024-08-23 03:24:19 +0530

    blog-website

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import Button from './Button'
+import RTE from './RTE'
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
+import Select from "./Select"
+
+
+export{
+    Header,LogoutBtn,Signup,Login,PostCard,Input,Select,
+    Footer,Container,Logo,Button,RTE,PostForm,AuthLayout
+}
